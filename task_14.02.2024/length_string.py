c81049aae1ba5afb04ebf071a262387fa53bd8dc
ALBIN KURIACHAN <140505145+Albin-Kuriachan@users.noreply.github.com>
2024-02-14 14:39:45 +0530

    Create length_string.py

---
+++ task_14.02.2024/length_string.py
@@ -0,0 +1,10 @@
+
+# Write a Python program to calculate the length of a string.
+
+
+def find_length(s):
+    return len(s)
+
+string = "Hello World"
+length = find_length(string)
+print(f"The length of the string is: {length}")
